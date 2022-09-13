ee6d7b48b8e3b7d0d25620ce8a225d54d32458fd
Ruslan <rusvas012@gmail.com>
2022-09-13 10:30:48 +0300

    lab 1 - task 1

---
+++ lab1/Triangle/Triangle/Program.cs
@@ -0,0 +1,61 @@
+﻿var triangleType = TriangleType.NotTriangle;
+
+try
+{
+    if (args.Length != 3) throw new ArgumentException();
+    var sidesOfTriangle = args.Select(x => double.Parse(x)).ToList();
+
+    var a = sidesOfTriangle[0];
+    var b = sidesOfTriangle[1];
+    var c = sidesOfTriangle[2];
+
+    if (!IsValidTriangle(a, b, c)) throw new ArgumentException();
+    triangleType = TriangleType.Triangle;
+
+    if (IsEquilateralTriangle(a, b, c))
+    {
+        triangleType = TriangleType.Equilateral;
+    }
+
+    if (IsIsoscelesTriangle(a, b, c))
+    {
+        triangleType = TriangleType.Isosceles;
+    }
+
+    Console.WriteLine(triangleType.ToString());
+}
+catch (Exception)
+{
+    Console.WriteLine("unknown error");
+    return;
+}
+
+#region utils
+bool IsValidTriangle(double a, double b, double c)
+{
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool IsEquilateralTriangle(double a, double b, double c)
+{
+    return (a == b && a == c);
+}
+
+bool IsIsoscelesTriangle(double a, double b, double c)
+{
+    return (a == b && a != c || a == c && a != b || b == c && b != a);
+}
+#endregion
+
+enum TriangleType
+{
+    NotTriangle,
+    Triangle,
+    Equilateral,
+    Isosceles,
+}
